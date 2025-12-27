a07ade9bd304df539e3358912dde76821568dde1
Alchemmist <anton.ingrish@gmail.com>
2025-12-27 08:38:25 +0300

    solve 028: O(n)

---
+++ 028-find-index-of-first-occurence.go
@@ -0,0 +1,18 @@
+//go:build ignore
+
+package main
+
+import "fmt"
+
+func strStr(haystack string, needle string) int {
+	for i := 0; i < len(haystack) - len(needle) + 1; i++ {
+		if haystack[i:i + len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(strStr("sodbutsad", "sad"))
+}
