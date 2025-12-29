ea6c835259f80e9ad24cd0bfe5e30e41c65d28cb
Alchemmist <anton.ingrish@gmail.com>
2025-12-29 09:18:12 +0300

    improve 035: O(log n)

--- 035-search-insert-position.go
+++ 035-search-insert-position.go
@@ -5,19 +5,23 @@ package main
 import "fmt"
 
 func searchInsert(nums []int, target int) int {
-	res := len(nums)
-	for i, n := range nums {
-		if n == target {
+	left := 0
+	right := len(nums)
+	for left < right {
+		i := left + (right-left)/2
+		if nums[i] == target {
 			return i
 		}
-		if target < n {
-			res = min(i, res)
+		if target < nums[i] {
+			right = i
+		} else {
+			left = i + 1
 		}
 	}
-	return res
+	return left
 }
 
 func main() {
 	data := []int{1, 3, 5, 6}
-	fmt.Println(searchInsert(data, 5))
+	fmt.Println(searchInsert(data, 6))
 }
