8ba7a6093adee69bf25cb708cf48acf715e36b01
Alchemmist <anton.ingrish@gmail.com>
2026-01-03 09:50:00 +0300

    solve 005: O(n^2)

--- 005-longest-palindromic-substring.go
+++ 005-longest-palindromic-substring.go
@@ -9,14 +9,26 @@ func longestPalindrome(s string) string {
 	for _, chr := range s {
 		s_odd += string(chr) + "|"
 	}
-	for i := 0; i < len(s_odd); i++ {
-		shift := 0
-		for i + shift < len(s_odd) && 0 <= i - shift {
-			if s_odd[i + shift] == s_odd[i - shift] {
 
+	maxPal := ""
+	for i := 0; i < len(s_odd); i++ {
+		for shift := 0; i+shift < len(s_odd) && 0 <= i-shift; shift++ {
+			if s_odd[i+shift] == s_odd[i-shift] {
+				if len(s_odd[i-shift:i+shift+1]) > len(maxPal) {
+					maxPal = s_odd[i-shift : i+shift+1]
+				}
+			} else {
+				break
 			}
 		}
 	}
+	res := ""
+	for _, letter := range maxPal {
+		if letter != '|' {
+			res = res + string(letter)
+		}
+	}
+	return res
 }
 
 func main() {
