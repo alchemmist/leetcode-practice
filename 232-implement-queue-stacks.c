82e023c4c93eae5a987538fd8297384b14e986c6
Alchemmist <anton.ingrish@gmail.com>
2026-01-19 09:57:55 +0300

    solve 232: O(n)

--- 232-implement-queue-stacks.c
+++ 232-implement-queue-stacks.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node {
     int value;
@@ -8,42 +9,62 @@ typedef struct Node {
 
 typedef struct {
     int length;
-    Node *top;
+    int capacity;
+    int *stack;
 } MyQueue;
 
 MyQueue *myQueueCreate() {
-    MyQueue *m;
+    MyQueue *m = calloc(1, sizeof(*m));
     m->length = 0;
+    m->capacity = 1;
+    m->stack = calloc(m->capacity, sizeof(int));
     return m;
 }
 
 void myQueuePush(MyQueue *obj, int x) {
-    if (obj->top == NULL) {}
+    if (obj->capacity - obj->length <= 1) {
+        obj->capacity *= 2;
+        obj->stack = realloc(obj->stack, obj->capacity * sizeof(*obj->stack));
+    }
+    obj->stack[obj->length] = x;
+    obj->length++;
 }
 
 int myQueuePop(MyQueue *obj) {
+    if (obj->length <= 0) {
+        return -1;
+    }
+
+    int value = obj->stack[0];
+    for (int i = 1; i < obj->length; i++) {
+        obj->stack[i - 1] = obj->stack[i];
+    }
+    obj->length--;
+
+    return value;
 }
 
 int myQueuePeek(MyQueue *obj) {
-    return obj->top->value;
+    if (obj->length <= 0) {
+        return -1;
+    }
+    return obj->stack[0];
 }
 
 bool myQueueEmpty(MyQueue *obj) {
+    return (obj->length == 0);
 }
 
 void myQueueFree(MyQueue *obj) {
+    free(obj->stack);
+    free(obj);
 }
 
-/**
- * Your MyQueue struct will be instantiated and called as such:
- * MyQueue* obj = myQueueCreate();
- * myQueuePush(obj, x);
-
- * int param_2 = myQueuePop(obj);
-
- * int param_3 = myQueuePeek(obj);
-
- * bool param_4 = myQueueEmpty(obj);
-
- * myQueueFree(obj);
-*/
+int main() {
+    MyQueue *m = myQueueCreate();
+    myQueuePush(m, 1);
+    myQueuePush(m, 2);
+    printf("%d\n", myQueuePeek(m));
+    printf("%d\n", myQueuePop(m));
+    printf("%b\n", myQueueEmpty(m));
+}
