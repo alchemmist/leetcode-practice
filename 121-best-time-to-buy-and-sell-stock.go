b467d8354b1cb63c3fc836a034691215ce494e22
Alchemmist <anton.ingrish@gmail.com>
2026-01-21 18:41:34 +0300

    solve 121: O(n)

---
+++ 121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,23 @@
+//package:build ignore
+
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+    mini := prices[0]
+    maxi := 0
+    for _, p := range prices {
+        if p < mini {
+            mini = p
+        }
+        if p - mini > maxi {
+            maxi = p - mini
+        }
+    }
+    return maxi
+}
+
+func main() {
+    fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+}
