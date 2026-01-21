1128db5aefc69b69893f85e519b23c6c88b0da89
Alchemmist <anton.ingrish@gmail.com>
2026-01-21 19:25:39 +0300

    solve 344: O(n)

---
+++ 344-reverse-string.go
@@ -0,0 +1,21 @@
+//package:build ignore
+
+package main
+
+import "fmt"
+
+func reverseString(s []byte)  {
+    l := 0
+    r := len(s) - 1
+    for range len(s) / 2 {
+        s[l], s[r] = s[r], s[l]
+        l++
+        r--
+    }
+}
+
+func main() {
+    a := []byte{'h', 'e', 'l', 'l', 'o'}
+    reverseString(a)
+    fmt.Printf("%s\n", a)
+}
