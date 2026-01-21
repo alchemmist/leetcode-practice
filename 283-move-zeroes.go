02c1cad6a5093bbc2c2512bde035096f6caca543
Alchemmist <anton.ingrish@gmail.com>
2026-01-21 20:19:50 +0300

    solve 283: O(n)

---
+++ 283-move-zeroes.go
@@ -0,0 +1,25 @@
+//package:build ignore
+
+package main
+
+import "fmt"
+
+func moveZeroes(nums []int)  {
+    write := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[write] = nums[i]
+            write++
+        }
+    }
+    for write < len(nums) {
+        nums[write] = 0
+        write++
+    }
+}
+
+func main() {
+    a := []int{0,0,1,3,12}
+    moveZeroes(a)
+    fmt.Println(a)
+}
