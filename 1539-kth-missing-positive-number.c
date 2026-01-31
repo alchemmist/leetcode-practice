7a40d6b1fdfa80b785ef6c14b44363f2ca01a74b
alchemmist <anton.ingrish@gmail.com>
2026-01-31 13:32:23 +0300

    improve 1539: O(n)

--- 1539-kth-missing-positive-number.c
+++ 1539-kth-missing-positive-number.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int findKthPositive(int *arr, int arrSize, int k) {
-    int missed = 0;
-    int last = 0;
-    for (int i = 0; i < arrSize; i++) {
-        int gap = arr[i] - last - 1;
-        if (missed + gap >= k) {
-            return last + (k - missed);
+    int left = 0;
+    int right = arrSize - 1;
+    while (left <= right) {
+        int i = (left + right) / 2;
+        if (arr[i] - (i + 1) >= k) {
+            right = i - 1;
+        } else {
+            left = i + 1;
         }
-        missed += gap;
-        last = arr[i];
     }
-    return arr[arrSize - 1] + (k - missed);
+    return left + k;
 }
 
 int main() {
-    int nums[] = {1, 3};
-    printf("%d\n", findKthPositive(nums, sizeof(nums) / sizeof(nums[0]), 3));
+    int nums[] = {2};
+    printf("%d\n", findKthPositive(nums, sizeof(nums) / sizeof(nums[0]), 1));
 }
-
