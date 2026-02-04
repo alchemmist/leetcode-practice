6649a48391bd263185ca1f8a4486bca6f1bc447f
alchemmist <anton.ingrish@gmail.com>
2026-02-04 21:43:39 +0300

    improve 34: O(log n)

--- 34-find-first-and-last-in-sorted-arr.go
+++ 34-find-first-and-last-in-sorted-arr.go
@@ -6,34 +6,48 @@ import (
 	"fmt"
 )
 
-func searchRange(nums []int, target int) []int {
-	l, r := 0, len(nums)-1
-	leftFinded, rightFinded := false, false
-	for l <= r {
+func lowerBound(nums []int, target int) int {
+	if len(nums) == 1 {
+		return 0
+	}
+	l, r := 0, len(nums)
+	for l < r {
 		i := (l + r) / 2
-		if nums[i] == target {
-			if nums[l] != target {
-				l++
-			} else {
-				leftFinded = true
-			}
-			if nums[r] != target {
-				r--
-			} else {
-				rightFinded = true
-			}
-			if leftFinded && rightFinded {
-				return []int{l, r}
-			}
+		if nums[i] < target {
+			l = i + 1
 		} else {
-			if nums[i] < target {
-				l = i + 1
-			} else {
-				r = i - 1
-			}
+			r = i
 		}
 	}
-	return []int{-1, -1}
+	return l
+}
+
+func upperBound(nums []int, target int) int {
+	l, r := 0, len(nums)
+	for l < r {
+		i := (l + r) / 2
+		if nums[i] <= target {
+			l = i + 1
+		} else {
+			r = i
+		}
+	}
+	return l
+}
+
+func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+
+	first := lowerBound(nums, target)
+
+	if first == len(nums) || nums[first] != target {
+		return []int{-1, -1}
+	}
+
+	last := upperBound(nums, target) - 1
+	return []int{first, last}
 }
 
 func main() {
